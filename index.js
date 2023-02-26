e9f1c71e4296f1bf3572a7f8686495f9ae91d231
SkyBluePenguin53 <82796132+SkyBluePenguin53@users.noreply.github.com>
2023-02-26 18:24:49 +0800

    Skeleton of Both Top and Side Navigation Bars
    Files include (all incomplete):
    
    home.ejs
    
    head.ejs
    top-nav-bar.ejs
    side-nav-bar.ejs
    
    styles.css

--- index.js
+++ index.js
@@ -5,7 +5,18 @@
     Date Created: February 25, 2023
 */
 
+/* Environment Variables */
 
+// Port value
+port = 8080
+
+
+
+
+
+
+
+/* Main Code */
 
 // Importing of ExpressJS
 var exprs = require('express');
@@ -13,16 +24,23 @@ var exprs = require('express');
 // Creating ExpressJS app
 var app = exprs();
 
+// Set view engine as EJS
+app.set('view engine', 'ejs');
+
+// App uses CSS (under construction)
+app.use("/css",exprs.static(__dirname + "/css"));
+
 // Get request from route '/' and callback function request(req) and response(res)
 // req represents the HTTP request
 // res represents the HTTP response
 app.get('/', function(req,res)
 {
-    // HTTP send response
-    res.send("Hello world!");
+    // HTTP render response
+    //res.render('pages/home');
+    res.render('pages/home');
 });
 
 // Binds and listens for connection on specified host and port.
-// Port here is 3000. Might change in the future.
 // Full syntax: app.listen(port, [host], [backlog], [callback]])
-app.listen(3000);
+app.listen(port);
+console.log('Server is active on port ' + port + '.');

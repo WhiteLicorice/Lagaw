8fa985bb751fec54fd7edf563f9052dbc9504de5
cmpdleon <cmpdleon@gmail.com>
2023-03-06 01:50:21 +0800

    Changes in light-dark-mode.js and affected ejs.
    Changed to single color-theme element instead of light and dark in top-nav-bar.ejs.
    Changed to switching source image instead of hidden and visible tags in js.

--- public/js/light-dark-mode.js
+++ public/js/light-dark-mode.js
@@ -1,43 +1,28 @@
-/*
-    Code adapted from:
-        https://stackoverflow.com/questions/67662216/dark-light-mode-button
-*/
-
 // Get navigation bar class members from html.
-top = document.getElementById("top-nav-bar");
-side = document.getElementById("side-nav-bar");
-light = document.getElementById("light");
-dark = document.getElementById("dark");
+topNav = document.getElementById("top-nav-bar"); //?? Error when variable is named top
+sideNav = document.getElementById("side-nav-bar");
+theme = document.getElementById("color-theme");
+icon = document.getElementById("theme-icon");
+
+darkMode = true; // Webpage is dark mode by default
 
 
 function lightDarkMode()
+    //TODO: Change element colors when swithcing modes
 {
-    // If page is in light mode
-    if (light.style.visibility == "hidden")
+    if (darkMode == true)
     {
-        // Switch button to light mode
-        light.style.visibility = "visible";
-        light.style.display = "inline";
-        dark.style.visibility = "hidden";
-        dark.style.display = "none";
-
-        // Make selected elements as dark.
-        top.style.background = 0x1B1B1B;
-        side.style.background = 0x1B1B1B;
-        
+        darkMode = false;
+        icon.src = "/img/dark.png"
+        topNav.style.background = "#FFFFFF";
+        sideNav.style.background = "#FFFFFF";
     }
     else
-    {   
-        // Switch button to dark mode
-        light.style.visibility = "hidden";
-        light.style.display = "none";
-        dark.style.visibility = "visible";
-        dark.style.display = "inline";
-
-        // Make selected elements as light.
-        top.style.background = 0xFFFFFF;
-        side.style.background = 0xFFFFFF;
-        
+    {
+        darkMode = true;
+        icon.src = "/img/light.png";
+        topNav.style.background = "#1B1B1B";
+        sideNav.style.background = "#1B1B1B";
     }
 }
 

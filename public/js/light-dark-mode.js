d6ff87e67565bea19ccfb372c06de6be89469b78
SkyBluePenguin53 <82796132+SkyBluePenguin53@users.noreply.github.com>
2023-03-07 14:04:45 +0800

    Improvements in light-dark-mode.js
    Improved text in light-dark-mode.js
    
    Co-Authored-By: WhiteLicorice <96515086+WhiteLicorice@users.noreply.github.com>

--- public/js/light-dark-mode.js
+++ public/js/light-dark-mode.js
@@ -2,6 +2,7 @@
 topNav = document.getElementById("top-nav-bar"); //?? Error when variable is named top
 sideNav = document.getElementById("side-nav-bar");
 fontWhite = document.querySelectorAll('.font-white')
+fontPeach = document.querySelectorAll('.font-peach')
 sideChildren = document.querySelectorAll('.side-child')
 theme = document.getElementById("color-theme");
 icon = document.getElementById("theme-icon");
@@ -15,23 +16,26 @@ function lightDarkMode()
     if (darkMode == true)
     {
         darkMode = false;
-        icon.src = "/img/dark.png"
+        icon.src = "/img/dark.png";
         topNav.style.background = "#FFFFFF";
         sideNav.style.background = "#FFFFFF";
-        
-        fontWhite.forEach(node => {
-            node.style.color = 'black'
-          })
+        document.getElementById('menu-btn').style.filter = 'invert(1)';
+        document.getElementById('search-bar-input').style.background = '#FDC28B';
+
+
+        fontWhite.forEach(node => 
+        {
+            node.style.filter = 'invert(1)'
+        })
+
+        fontPeach.forEach(node =>
+        {
+            node.style.filter = 'invert(1)'
+        })
+
         sideChildren.forEach(node => {
             node.style.webkitFilter = 'invert(1)'
         })
-
-        document.getElementById('menu-btn').style.filter = 'invert(1)';
-
-        
-        document.getElementById('search-bar-input').style.background = '#FDC28B';
-
-        
     }
     else
     {
@@ -39,17 +43,23 @@ function lightDarkMode()
         icon.src = "/img/light.png";
         topNav.style.background = "#1B1B1B";
         sideNav.style.background = "#1B1B1B";
-
-        fontWhite.forEach(node => {
-            node.style.color = 'white'
-          })
-
-        sideChildren.forEach(node => {
-            node.style.webkitFilter = 'invert(0)'
+        document.getElementById('menu-btn').style.filter = 'invert(0)';
+        document.getElementById('search-bar-input').style.background = '#FFFFFF';
+        
+        fontWhite.forEach(node => 
+        {
+            node.style.filter = 'invert(0)'
         })
 
-        document.getElementById('search-bar-input').style.background = '#FFFFFF';
-        document.getElementById('menu-btn').style.filter = 'invert(0)';
+        fontPeach.forEach(node =>
+        {
+            node.style.filter = 'invert(0)'
+        })
+
+        sideChildren.forEach(
+            node => {
+            node.style.filter = 'invert(0)'
+        })
     }
 }
 

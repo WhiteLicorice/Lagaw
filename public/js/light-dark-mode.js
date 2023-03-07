3d52353a6388cb44babb96c4b3bcebce0dccf2d4
SkyBluePenguin53 <82796132+SkyBluePenguin53@users.noreply.github.com>
2023-03-07 10:11:21 +0800

    Update light-dark-mode.js
    Refactored dark-light mode text.
    
    Co-Authored-By: WhiteLicorice <96515086+WhiteLicorice@users.noreply.github.com>

--- public/js/light-dark-mode.js
+++ public/js/light-dark-mode.js
@@ -1,7 +1,7 @@
 // Get navigation bar class members from html.
 topNav = document.getElementById("top-nav-bar"); //?? Error when variable is named top
 sideNav = document.getElementById("side-nav-bar");
-fontWhite = document.getElementsByClassName("font-white");
+fontWhite = document.querySelectorAll('.font-white')
 theme = document.getElementById("color-theme");
 icon = document.getElementById("theme-icon");
 
@@ -18,9 +18,12 @@ function lightDarkMode()
         topNav.style.background = "#FFFFFF";
         sideNav.style.background = "#FFFFFF";
         
-        Array.prototype.forEach.call(fontWhite, function(element){
-            element.color = "#000000";
-        })
+        fontWhite.forEach(node => {
+            node.style.color = 'black'
+          })
+
+
+        
     }
     else
     {
@@ -28,9 +31,10 @@ function lightDarkMode()
         icon.src = "/img/light.png";
         topNav.style.background = "#1B1B1B";
         sideNav.style.background = "#1B1B1B";
-        Array.prototype.forEach.call(fontWhite, function(element){
-            element.color = "#EAF4FE";
-        })
+
+        fontWhite.forEach(node => {
+            node.style.color = 'white'
+          })
     }
 }
 

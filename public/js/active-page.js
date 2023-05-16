cc4df7db4474183b74921469b4b91dbe20b62bdf
SkyBluePenguin53 <82796132+SkyBluePenguin53@users.noreply.github.com>
2023-05-16 13:22:04 +0800

    Side bar improvements part 1
    In sidebar,
    - height is set to window height
    - sidebar is placed in front of top nav bar
    - Putting of menu bar button to open or collapse side bar
    
    Ongoing construction:
    - Black transparent screen to focus on the active sidebar
    - Highlight of sidebar link when its page is active

--- public/js/active-page.js
+++ public/js/active-page.js
@@ -1,7 +1,29 @@
-const active = window.location.pathname;
+/* const active = window.location.pathname;
 const links = document.querySelectorAll('nav li a').
 forEach(link => {
     if(link.href.includes('${active}')){
         link.classList.add('active');
     }
-});
+}); */
+
+var currentURL = window.location.href;
+
+var pageNames = ["http://localhost:8080/places-and-landmarks", "http://localhost:8080/culture-and-festivals", "http://localhost:8080/food", "http://localhost:8080/accommodation", "http://localhost:8080/traffic"];
+
+for (var index = 1; index <= pageNames.length; i++) 
+{
+  var link = document.getElementById("link" + i);
+  var image = link.getElementsByTagName("img");
+  var text = link.getElementsByTagName("p");
+
+  if (currentURL.includes(pageNames[i])) 
+  {
+    image.style.color = "#b57e07";
+    text.style.color = "#b57e07";
+  }
+  else
+  {
+    image.style.color = "white";
+    text.style.color = "white";
+  }
+}
--- public/js/side-nav-bar.js
+++ public/js/side-nav-bar.js
@@ -1,13 +1,22 @@
-side = document.getElementById("side-nav-bar");
+const side = document.getElementById('side-nav-bar');
+const slideAmount = -100;
+let transformed = false;
 
-function showHide()
-{
-    if(side.style.visibility == "visible")
-    {
-        side.style.visibility = "hidden";
+function showHide() {
+    /* if (side.style.transform == 'translateX(-100px)') {
+        side.style.transform = 'translateX(0)';
     }
-    else
+    else {
+        side.style.transform = 'translateX(-100px)';
+    } */
+    if (transformed) 
     {
-        side.style.visibility = "visible";
+        side.style.transform = `translateX(${slideAmount}px)`;
+    } 
+    else 
+    {
+        side.style.transform = 'translateX(0)';
     }
+
+    transformed = !transformed;
 }

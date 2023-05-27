f1ab2c142f55d896005fe75f8ab48d49e08f81cd
SkyBluePenguin53 <82796132+SkyBluePenguin53@users.noreply.github.com>
2023-05-27 19:51:32 +0800

    Additions in Light/Dark Mode
    Good news
    - This mode works on Homepage
    
    Bad news
    - This mode partially works on the rest of the applied pages (pls fix)

--- public/js/light-dark-mode.js
+++ public/js/light-dark-mode.js
@@ -2,94 +2,96 @@
 topNav = document.getElementById("top-nav-bar");
 sideNav = document.getElementById("side-nav-bar");
 fontWhite = document.querySelectorAll('.font-white')
+fontBlack = document.querySelectorAll('.font-black')
 fontPeach = document.querySelectorAll('.font-peach')
 sideChildren = document.querySelectorAll('.side-child')
 theme = document.getElementById("color-theme");
 icon = document.getElementById("theme-icon");
-transparentBackground = document.querySelectorAll('transparent-background-home');
-transparentBackgroundLeft = document.querySelectorAll('transparent-background-left');
-transparentBackgroundTitle = document.querySelectorAll("transparent-background-title");
+menuBtn = document.getElementById("menu");
+homeHighlights = document.getElementById('home-highlights');
+homeExplore = document.getElementById('home-explore');
+transparentBackgroundHome = document.querySelectorAll('.transparent-background-home');
+transparentBackgroundPaL = document.querySelectorAll('.transparent-background-p-a-l');
+transparentBackgroundTitle = document.querySelectorAll(".transparent-background-title");
 
 darkMode = true; // Webpage is dark mode by default
 function lightDarkMode()
-    //TODO: Change element colors when swithcing modes
+//TODO: Change element colors when swithcing modes
 {
-    if (darkMode == true)
-    {
+    if (darkMode == true) {
         darkMode = false;
         icon.src = "/img/dark.png";
         topNav.style.background = "#FFFFFF";
         sideNav.style.background = "#FFFFFF";
-        document.getElementById('menu-btn').style.filter = 'invert(1)';
+        menuBtn.style.filter = 'invert(1)';
         document.getElementById('search-bar-input').style.background = '#b57e07';
+        homeHighlights.style.background = "linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), url('/img/whitebg.jpg')";
+        homeExplore.style.background = "linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), url('/img/whitebg.jpg')";
 
-
-        transparentBackground.forEach(bg => 
-        {
-            bg.style.backgroundColor = "rgba(237,231,225,0.7)";
+        transparentBackgroundHome.forEach(bg => {
+            bg.style.background = "rgba(237,231,225,0.7)";
         })
 
-        transparentBackgroundLeft.forEach(bgl => 
-        {
+        transparentBackgroundPaL.forEach(bgl => {
             bgl.style.backgroundColor = "rgba(237,231,225,0.7)";
         })
 
-        transparentBackgroundTitle.forEach(bgt => 
-        {
+        transparentBackgroundTitle.forEach(bgt => {
             bgt.style.backgroundColor = "rgba(237,231,225,0.7)";
         })
 
-        fontWhite.forEach(whitefont => 
-        {
+        fontWhite.forEach(whitefont => {
             whitefont.style.filter = 'invert(1)'
         })
 
-        fontPeach.forEach(whitefont =>
-        {
+        fontBlack.forEach(whitefont => {
             whitefont.style.filter = 'invert(1)'
         })
 
-        sideChildren.forEach(child => 
-        {
+        fontPeach.forEach(whitefont => {
+            whitefont.style.filter = 'invert(1)'
+        })
+
+        sideChildren.forEach(child => {
             child.style.webkitFilter = 'invert(1)'
         })
     }
-    else
-    {
+    else {
         darkMode = true;
         icon.src = "/img/light.png";
         topNav.style.background = "#1B1B1B";
         sideNav.style.background = "#1B1B1B";
-        document.getElementById('menu-btn').style.filter = 'invert(0)';
+        menuBtn.style.filter = 'invert(0)';
         document.getElementById('search-bar-input').style.background = '#FFFFFF';
+        homeHighlights.style.background = "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/img/whitebg.jpg')";
+        homeExplore.style.background = "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/img/whitebg.jpg')";
 
-        transparentBackground.forEach(bg => 
-        {
-            bg.style.backgroundColor = "rgba(0,0,0,0.7)";
+
+        transparentBackgroundHome.forEach(bg => {
+            bg.style.background = "rgba(0,0,0,0.7)";
         })
 
-        transparentBackgroundLeft.forEach(bgl => 
-        {
+        transparentBackgroundPaL.forEach(bgl => {
             bgl.style.backgroundColor = "rgba(0,0,0,0.7)";
         })
 
-        transparentBackgroundTitle.forEach(bgt => 
-        {
+        transparentBackgroundTitle.forEach(bgt => {
             bgt.style.backgroundColor = "rgba(0,0,0,0.7)";
         })
 
-        fontWhite.forEach(node => 
-        {
+        fontWhite.forEach(node => {
             node.style.filter = 'invert(0)'
         })
 
-        fontPeach.forEach(node =>
-        {
+        fontBlack.forEach(node => {
             node.style.filter = 'invert(0)'
         })
 
-        sideChildren.forEach(node =>
-        {
+        fontPeach.forEach(node => {
+            node.style.filter = 'invert(0)'
+        })
+
+        sideChildren.forEach(node => {
             node.style.filter = 'invert(0)'
         })
     }
